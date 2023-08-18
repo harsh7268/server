8892f676d44da4e73ab474de7ff0c46d3c5c1920
harsh7268 <harshpatel726891@gmail.com>
2023-08-18 16:53:06 +0530

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'password',
+    socketPath: '/var/run/mysqld/mysqld.sock',
     database: '92lottery'
 });
 
